0ef57fd6d4951a4bf90a96c4311292374f2975ae
Rajendra Sarathe <143543512+rajendrasarathe7070@users.noreply.github.com>
2026-07-09 13:48:20 +0000

    raj123

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
+
+# install dependencies
 pip install -r requirements.txt
+
+# collect static files
 python manage.py collectstatic --no-input
+
+# migrate database
 python manage.py migrate
