1d1dad36076f1ea790da15f0473ef1fa5729fc3a
Rajendra Sarathe <143543512+rajendrasarathe7070@users.noreply.github.com>
2026-07-11 07:08:20 +0000

    fix 1.5

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ python manage.py collectstatic --no-input
 
 # 3. डेटाबेस माइग्रेशन चलाएं (ताकि core_note टेबल वाली एरर ठीक हो सके)
 echo "Running database migrations..."
-python src/manage.py migrate
+python manage.py migrate
 
 echo "Build process completed successfully!"
