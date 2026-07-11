b41e6dc6528bc870f5f1eaa3657257c2ccd85736
Rajendra Sarathe <143543512+rajendrasarathe7070@users.noreply.github.com>
2026-07-11 06:59:26 +0000

    fix 1.2

--- build.sh
+++ build.sh
@@ -5,5 +5,12 @@ set -o errexit
 # install dependencies
 pip install -r requirements.txt
 
-# collect static files
-python manage.py collectstatic --no-input
+
+echo "Collecting static files..."
+python src/manage.py collectstatic --no-input
+
+# 3. डेटाबेस माइग्रेशन चलाएं (ताकि core_note टेबल वाली एरर ठीक हो सके)
+echo "Running database migrations..."
+python src/manage.py migrate
+
+echo "Build process completed successfully!"
